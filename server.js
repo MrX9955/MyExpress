b70d70e4e959505a06ee8bc1b3760dfe84e32eb1
Deepthi <deepthi.2963@gmail.com>
2025-10-13 15:41:31 +0530

    vikash

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let books = [];
+
+
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+app.post('/books', (req, res) => {
+  const { title, author } = req.body;
+
+  if (!title || !author) {
+    return res.status(400).json({ message: 'Title and author are required.' });
+  }
+
+  const newBook = {
+    id: books.length + 1,
+    title,
+    author
+  };
+
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
