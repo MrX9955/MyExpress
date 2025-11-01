ac088756cc54dc6d9212629b476b8a8b6c88d659
VIKASH KUMAR SAH(MR.X) <231fa04f67@gmail.com>
2025-11-01 20:25:26 +0530

    Remove MongoDB connection code
    Removed MongoDB connection code from server.js.

--- server.js
+++ server.js
@@ -25,15 +25,6 @@ app.use(
   })
 );
 
-// 🧩 MongoDB Connection
-mongoose
-  .connect("mongodb+srv://mrx9955:Bihar9955@vikashdb.3e0vafd.mongodb.net/BloodDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB Connection Error: " + err));
-
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
