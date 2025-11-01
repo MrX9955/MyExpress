6d4374d0d442693bb5579a1d014171a398e675ff
VIKASH KUMAR SAH(MR.X) <231fa04f67@gmail.com>
2025-11-01 20:30:07 +0530

    Integrate MongoDB connection in server.js
    Added MongoDB connection using mongoose.

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+mongoose
+  .connect("mongodb+srv://mrx9955:Bihar9955@vikashdb.3e0vafd.mongodb.net/BloodDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB Connection Error: " + err));
+
 
 app.get('/', (req, res) => {
 res.send('Welcome to the Book Library.');
