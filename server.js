51a4eade13007216c7aeeef9d02c9f12e924ed28
VIKASH KUMAR SAH(MR.X) <231fa04f67@gmail.com>
2025-11-01 20:28:56 +0530

    Remove BookStore data and related endpoint
    Removed the BookStore array and its associated endpoint.

--- server.js
+++ server.js
@@ -3,18 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-const BookStore = [
-{ id: 1, name: "The White Tiger", author: "Aravind Adiga" },
-{ id: 2, name: "The God of Small Things", author: "Arundhati Roy" },
-{ id: 3, name: "Train to Pakistan", author: "Khushwant Singh" },
-{ id: 4, name: "The Palace of Illusions", author: "Chitra Banerjee Divakaruni" },
-{ id: 5, name: "The Immortals of Meluha", author: "Amish Tripathi" },
-];
-
-// ✅ Fixed variable name here
-app.get('/books', (req, res) => {
-res.json(BookStore);
-});
 
 app.get('/', (req, res) => {
 res.send('Welcome to the Book Library.');
