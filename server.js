c9d9a30ca767f5511266268554fabe2e27bf1a0e
VIKASH KUMAR SAH(MR.X) <231fa04f67@gmail.com>
2025-11-01 20:33:45 +0530

    Refactor MongoDB connection handling
    Refactor MongoDB connection logic to use an async function and check connection status before handling requests.

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const app = express();
 
 app.use(express.json());
 
-mongoose
-  .connect("mongodb+srv://mrx9955:Bihar9955@vikashdb.3e0vafd.mongodb.net/BloodDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB Connection Error: " + err));
+// 🧩 MongoDB Connection
+let isConnected = false;
+async function ConnectedToDB() {
+      mongoose.connect("mongodb+srv://mrx9955:Bihar9955@vikashdb.3e0vafd.mongodb.net/BloodDB", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      isConnected = true
+      console.log("MongoDB Connected")
+    })
+    .catch(err => console.error("MongoDB Connection Error:", err));
+}
+
+// middleware 
+app.use((req,res,next)=>{
+  if(!isConnected){
+    ConnectedToDB();
+  }
+  next();
+})
 
 
 app.get('/', (req, res) => {
